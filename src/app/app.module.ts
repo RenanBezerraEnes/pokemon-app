0f80f30325ae94e3fca9ad8b312a4d22d9b47ad9
RenanBezerraEnes <renanbezerraenes@icloud.com>
2023-08-03 14:25:45 +0200

    It is always important to add the imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PokemonBaseModule } from './pokemon-base/pokemon-base.module';
+import { PokemonTemplateFormComponent } from './pokemon-template-form/pokemon-template-form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, PokemonBaseModule],
+  declarations: [AppComponent, PokemonTemplateFormComponent],
+  imports: [BrowserModule, PokemonBaseModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
