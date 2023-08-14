f49af33c519a3372ab5f833392d2f9d67f364804
RenanBezerraEnes <renanbezerraenes@icloud.com>
2023-08-14 12:31:34 +0200

    Moved pokemon form, to the pokemon folder

--- src/app/pokemon-base/pokemon-template-form/pokemon-template-form.component.ts
+++ src/app/pokemon-base/pokemon-template-form/pokemon-template-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { PokemonService } from '../services/pokemon-service.service';
-import { Pokemon, PokemonType } from '../models/pokemon';
+import { PokemonService } from '../../services/pokemon-service.service';
+import { Pokemon, PokemonType } from '../../models/pokemon';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-pokemon-template-form',
@@ -19,7 +20,11 @@ export class PokemonTemplateFormComponent implements OnInit {
       value: 'Water',
     },
   ];
-  constructor(private pokemonService: PokemonService) {}
+  constructor(
+    private pokemonService: PokemonService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   toggleIsCool(object: any) {
     console.log(object);
@@ -31,8 +36,17 @@ export class PokemonTemplateFormComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.pokemonService.getPokemonWithId(1).subscribe((data: Pokemon) => {
-      this.pokemon = data;
+    this.pokemon = {} as Pokemon;
+    this.route.params.subscribe((data: Params) => {
+      return this.pokemonService
+        .getPokemonWithId(data['id'])
+        .subscribe((data: Pokemon) => {
+          this.pokemon = data;
+        });
     });
+  }
+
+  back(): void {
+    this.router.navigate(['/pokemon']);
   }
 }
