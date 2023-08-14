1b306a760e23d2b2197042e308c615915efd87a2
RenanBezerraEnes <renanbezerraenes@icloud.com>
2023-08-14 12:32:49 +0200

    Routes links

--- src/app/pokemon-base/pokemon-base.module.ts
+++ src/app/pokemon-base/pokemon-base.module.ts
@@ -4,15 +4,37 @@ import { HttpClientModule } from '@angular/common/http';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
 import { PokemonService } from '../services/pokemon-service.service';
+import { PokemonTemplateFormComponent } from './pokemon-template-form/pokemon-template-form.component';
+import { Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: 'pokemon', component: PokemonListComponent },
+      { path: ':id', component: PokemonTemplateFormComponent },
+    ],
+  },
+];
 
 @NgModule({
   declarations: [
     PokemonListComponent,
     PokemonDetailComponent,
-    PokemonDetailComponent,
+    PokemonTemplateFormComponent,
   ],
-  imports: [CommonModule, HttpClientModule],
-  exports: [PokemonListComponent, PokemonDetailComponent],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forChild(routes),
+  ],
+  exports: [
+    PokemonListComponent,
+    PokemonDetailComponent,
+    PokemonTemplateFormComponent,
+  ],
   providers: [PokemonService],
 })
 export class PokemonBaseModule {}
